9edeac5f591db2b8e1dce0de1fdec026c3daa2a9
hamidbappah <91728007+hamidbappah@users.noreply.github.com>
2026-08-16 16:45:14 +0100

    Add types and example usage in types.py

---
+++ types.py
@@ -0,0 +1,8 @@
+name = "Amina"        # str
+age = 21              # int
+gpa = 4.32            # float
+enrolled = True       # bool
+nothing = None        # no value
+
+age = age + 1         # reassign
+print(type(gpa))
